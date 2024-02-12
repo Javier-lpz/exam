d8d5451b319dfbbb829077ff69656bc136c3b33e
Javier-lpz <javier.lopez.rico@gmail.com>
2024-02-12 19:12:17 +0100

    Entrega

---
+++ ProyectoExam/function.js
@@ -0,0 +1,22 @@
+
+//barras de habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("arcgis");
+        habilidades[3].classList.add("office");
+        habilidades[4].classList.add("comunicacion");
+        habilidades[5].classList.add("trabajo");
+        habilidades[6].classList.add("creatividad");
+        habilidades[7].classList.add("compromiso")
+    }
+}
+
+//efecto barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
